8934c8dd8c121023915b4db65e136e811378bec6
Mateusz Michalak <Mateusz Michalak>
2016-10-17 20:33:59 +0200

    Bind UI, a little bit of model

---
+++ Assets/Assets/Scripts/Data/Materiall.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewMaterial", menuName = "Data/Material")]
+public class Materiall : ScriptableObject
+{
+    public float ConductCoefficient = 0;
+}
---
+++ Assets/Assets/Scripts/Loader.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Loader : MonoBehaviour
+{
+
+    public Example[] Examples;
+    public Materiall[] Materials;
+
+    public Example activeExample;
+    public Materiall activeMaterial;
+    public float EnviromentTemperature;
+    public float ObjectTemperature;
+
+    public Dropdown Example;
+    public Dropdown Material;
+    public InputField EnviromentValue;
+    public InputField ObjectValue;
+
+
+    void Start()
+    {
+    }
+
+    public void LoadExample()
+    {
+
+    }
+
+    public void SetEnviromentTemperature()
+    {
+        float val;
+        try
+        {
+            float.TryParse(EnviromentValue.text, out val);
+            if (val > 300 || val < 60)
+            {
+                val = 60.0f;
+                EnviromentValue.text = "60.0";
+            }
+        }
+        catch (System.Exception)
+        {
+            EnviromentValue.text = "60.0";
+            val = 60.0f;
+        }
+        EnviromentTemperature = val;
+    }
+
+    public void LoadMaterial()
+    {
+
+    }
+
+    public void SetObjectTemperature()
+    {
+        float val = 0;
+        try
+        {
+            float.TryParse(ObjectValue.text, out val);
+            if (val > 35.0f || val < 0.0f)
+            {
+                val = 0.0f;
+                ObjectValue.text = "0.0";
+            }
+        }
+        catch (System.Exception)
+        {
+            val = 0;
+            ObjectValue.text = "0.0";
+        }
+
+        ObjectTemperature = val;
+    }
+
+}
