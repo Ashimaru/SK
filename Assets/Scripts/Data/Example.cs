e198cd4364d20f3f9b6972dd3b6e09844f523489
Mateusz Michalak <Mateusz Michalak>
2016-10-17 19:31:02 +0200

    First code

---
+++ Assets/Scripts/Data/Example.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[CreateAssetMenu(fileName = "newExample", menuName = "Data/Example")]
+public class Example : ScriptableObject {
+
+    public Sprite Sprite;
+    [SerializeField]
+    public Node[] Nodes;
+
+
+}
---
+++ Assets/Scripts/Loader.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Loader : MonoBehaviour {
+
+
+
+    public void LoadExample(int i)
+    {
+
+    }
+
+    public void SetTemperature(int value)
+    {
+
+    }
+
+    public void LoadMaterial(int i)
+    {
+
+    }
+
+    public void SetStartingTemperature(int i)
+    {
+
+    }
+
+}
