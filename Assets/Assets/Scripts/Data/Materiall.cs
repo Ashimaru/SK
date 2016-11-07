c189425f962002a2f168092d1a5759bda1af2672
Mateusz Michalak <Mateusz Michalak>
2016-11-07 22:14:06 +0100

    Fixed issu with math.NET

--- Assets/Assets/Scripts/Data/Materiall.cs
+++ Assets/Assets/Scripts/Data/Materiall.cs
@@ -1,10 +1,8 @@
 ﻿using System;
 using UnityEngine;
-using MathNet.Numerics;
 
 [CreateAssetMenu(fileName = "NewMaterial", menuName = "Data/Material")]
 public class Materiall : ScriptableObject
 {
-    
     public float ConductCoefficient = 0;
 }
--- Assets/Assets/Scripts/Data/Node.cs
+++ Assets/Assets/Scripts/Data/Node.cs
@@ -5,10 +5,11 @@ public class Node
 {
     public Vector2 Position;
     public float Temperature;
-
+    MathNet.Numerics.LinearAlgebra.Matrix<float> matr;
 
     public Node(Vector2 position) : this(position, 20.0f)
     {
+        matr = MathNet.Numerics.LinearAlgebra.Matrix<float>.Build.Diagonal(2, 2);
         Position = position;
     }
 
