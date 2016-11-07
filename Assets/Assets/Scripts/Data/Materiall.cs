6ca081d5197830de55bab4cf2ab2d1bdc879f227
Mateusz Michalak <Mateusz Michalak>
2016-11-07 20:02:35 +0100

    Imported Math.NET numerics

--- Assets/Assets/Scripts/Data/Materiall.cs
+++ Assets/Assets/Scripts/Data/Materiall.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using UnityEngine;
+using MathNet.Numerics;
 
 [CreateAssetMenu(fileName = "NewMaterial", menuName = "Data/Material")]
 public class Materiall : ScriptableObject
 {
+    
     public float ConductCoefficient = 0;
 }
