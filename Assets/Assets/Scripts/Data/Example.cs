068c7ad1aeef495f0c049a495bc135537b2e1017
Marcin Mazur <marmaz.966@o2.pl>
2016-11-21 13:49:12 +0100

    push

--- Assets/Assets/Scripts/Data/Example.cs
+++ Assets/Assets/Scripts/Data/Example.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
+using MathNet.Numerics.LinearAlgebra;
 
 [CreateAssetMenu(fileName = "newExample", menuName = "Data/Example")]
 public class Example : ScriptableObject
@@ -7,10 +8,14 @@ public class Example : ScriptableObject
 
     public Mesh Mesh;
     FiniteElement[] elements;
+    Matrix<float> GlobalStiffnessMatrix;
+    float flux;
+    float[] boundryConditions;
 
-    public void Load()
+    public void Load(Loader loader)
     {
         Materiall material = CreateInstance<Materiall>();
+        boundryConditions = new float[Mesh.vertexCount];
         material.ConductCoefficient = 5;
         elements = new FiniteElement[Mesh.triangles.Length / 3];
         for (int i = 0; i < elements.Length; i++)
@@ -24,16 +29,19 @@ public class Example : ScriptableObject
             elements[i] = new FiniteElement(nodes, material);
         }
 
-        Program program = new Program(Mesh.vertices.Length);
+        GlobalStiffnessMatrix = Program.Instance.AssembleGlobalStiffnessMatrix(elements, Mesh.vertices.Length);
+        flux = Program.Instance.CountFlux(material, loader.EnviromentTemperature);
 
-        program.AssembleGlobalStiffnessMatrix(elements);
+        var edges = Edge.GetEdges(Mesh.triangles);
+        var boundaries = Edge.GetBoudaries(edges);
+
+        boundryConditions = Program.Instance.BoundryConditions(flux, boundaries, Mesh.vertexCount, Mesh);
     }
 }
 
 public class Edge
 {
     public int Vertex1 { get; private set; }
-
     public int Vertex2 { get; private set; }
     public int TriangleNumber { get; private set; }
 
@@ -77,6 +85,17 @@ public class Edge
             }
         }
         return result;
+    }
+
+    public float Length(Mesh mesh)
+    {
+        float length = 0;
+        float a = mesh.vertices[Vertex1].x - mesh.vertices[Vertex2].x;
+        float b = mesh.vertices[Vertex2].y - mesh.vertices[Vertex2].y;
+
+        length = Mathf.Sqrt(a * a + b * b);
+
+        return length;
     }
 
 }
--- Assets/Assets/Scripts/Program.cs
+++ Assets/Assets/Scripts/Program.cs
@@ -2,19 +2,32 @@
 using System.Collections;
 using MathNet.Numerics.LinearAlgebra;
 using System;
+using System.Collections.Generic;
 
 public class Program
 {
-
-    public Matrix<float> GlobalStiffnessMatrix;
-
-    public Program(int nodesCount)
+    private static Program _instance;
+    public static Program Instance
     {
-        GlobalStiffnessMatrix = Matrix<float>.Build.Dense(nodesCount, nodesCount);
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = new Program();
+            }
+            return _instance;
+        }
     }
 
-    public void AssembleGlobalStiffnessMatrix(FiniteElement[] fe)
+    private Program()
     {
+
+    }
+
+    public Matrix<float> AssembleGlobalStiffnessMatrix(FiniteElement[] fe, int nodesCount)
+    {
+        Matrix<float> GlobalStiffnessMatrix = Matrix<float>.Build.Dense(nodesCount, nodesCount);
+
         for (int i = 0; i < fe.Length; i++)
         {
             for (int j = 0; j < 3; j++)
@@ -26,5 +39,30 @@ public class Program
                 }
             }
         }
+        return GlobalStiffnessMatrix;
+    }
+
+    public float CountFlux(Materiall material, float temperature)
+    {
+        float result = -material.ConductCoefficient;
+
+        return result * temperature;
+    }
+
+    public float[] BoundryConditions(float flux, List<Edge> boundaries, int nodesCount, Mesh mesh)
+    {
+        float[] boundryConditions = new float[nodesCount];
+        for(int i = 0; i < boundaries.Count; i++)
+        {
+            boundryConditions[boundaries[i].Vertex1] += (flux * boundaries[i].Length(mesh)) / 2;
+            boundryConditions[boundaries[i].Vertex2] += (flux * boundaries[i].Length(mesh)) / 2;
+        }
+
+        return boundryConditions;
+    }
+
+    public void CountingTemperatures(List<FiniteElement> fe)
+    {
+
     }
 }
